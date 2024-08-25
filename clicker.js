f3f8f8f31f3a17f5317885694cf6b904e521764f
oareyna <116768925+oareyna@users.noreply.github.com>
2024-08-25 16:23:33 -0400

    Add files via upload

---
+++ clicker.js
@@ -0,0 +1,8 @@
+clicks = "😊"
+
+document.getElementById("_count").innerHTML = clicks
+
+function clicked(){
+    clicks= clicks+"I love you "
+    document.getElementById("_count").innerHTML = clicks
+}
