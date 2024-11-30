e9c4b557447d6a113497dd51cd35604df3256f03
oareyna <116768925+oareyna@users.noreply.github.com>
2024-11-29 19:18:17 -0500

    Delete clicker.js

--- clicker.js
+++
@@ -1,8 +0,0 @@
-clicks = "😊"
-
-document.getElementById("_count").innerHTML = clicks
-
-function clicked(){
-    clicks= clicks+"I love you "
-    document.getElementById("_count").innerHTML = clicks
-}
